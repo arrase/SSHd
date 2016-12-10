35ad2bfaefcf2cbdcc6bdc99a6bdf92080c4ca32
arrase <arrase@gmail.com>
2016-12-10 17:23:00 +0100

    added new icon + daemon running notification

--- app/src/main/java/llanes/ezquerro/juan/cmsshd/service/SSHdService.java
+++ app/src/main/java/llanes/ezquerro/juan/cmsshd/service/SSHdService.java
@@ -1,12 +1,16 @@
 package llanes.ezquerro.juan.cmsshd.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import llanes.ezquerro.juan.cmsshd.R;
 import llanes.ezquerro.juan.cmsshd.constants.SSHdConstants;
 
 public class SSHdService extends Service {
@@ -20,7 +24,12 @@ public class SSHdService extends Service {
         final int id = startId;
 
         if (intent != null && intent.getAction().equals(SSHdConstants.ACTION_START)) {
+
             final String config_path = intent.getStringExtra(SSHdConstants.SSHD_CONFIG);
+
+            final NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            mNotificationManager.notify(id, getNotification());
 
             new Thread(new Runnable() {
                 public void run() {
@@ -38,6 +47,7 @@ public class SSHdService extends Service {
                         // Silent block
                     }
 
+                    mNotificationManager.cancel(id);
                     stopSelf(id);
                 }
             }).start();
@@ -49,5 +59,16 @@ public class SSHdService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private Notification getNotification() {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setOngoing(true)
+                        .setContentTitle(getString(R.string.sshd_is_running))
+                        .setContentText(getString(R.string.login_info));
+
+        return mBuilder.build();
     }
 }
